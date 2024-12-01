a544baed742fd8cb2a27c3b174181c00a161b2bb
Alex Matyko <alexa.ua@gmail.com>
2024-12-01 03:08:37 +0200

    Hex converted

--- ft_prnnum.c
+++ ft_prnnum.c
@@ -6,41 +6,42 @@ int printchar(char c)
 	return write(1,&c,1);
 }
 
-int printnum(int num, int base)
+int printnum(int num,const int base,const int upper)
 {
-    char    *numbers;
-    int c;
-    int cr;
+	char    *numbers;
+	int	c;
+	int	cr;
 
-    numbers = "0123456789ABCDEF";
-    if (num == 0) // Base case
-    {
-        c = printchar('0');
-        if (c == -1)
-        return (-1);
-        else
-        return (c);
-
-    }
-
-
-    cr = printnum(num / base, base);
-    if (cr == -1)
-        return (-1);
-    c = printchar(numbers[num % base]);
-        if (c == -1)
-        return (-1);
-    return (c + cr);
+	c = 0;
+	cr = 0;
+	if (num < 0)
+	{
+		c = printchar('-');
+		if (c == -1)
+    		return (-1);
+		num = -num;
+	}
+    numbers = "0123456789abcdef";
+	if (num > base)
+	   	cr = printnum(num / base, base, upper);
+	if (cr == -1)
+		return (-1);
+	if ((numbers[num % base]) >= 'a' && (numbers[num % base]) <= 'f' && upper )
+	c = printchar(numbers[num % base] - 32);
+	else
+	c = printchar(numbers[num % base]);
+    if (c == -1)
+    	return (-1);
+	return (c + cr);
 }
 
 int main()
 {
 
     int res;
-	printf("Hello World\n");
-	res = printnum(9,10);
-	dprintf(2,"%d\n",res);
 
-
+	res = printnum(255,16,1);
+	dprintf(2,"\n",1);
+	dprintf(2,"Printed %d\n",res);
 	return 0;
 }

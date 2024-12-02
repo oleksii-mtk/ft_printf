fa9b911fa8b217a7406aa3245110f8524428507f
Oleksii Matyko <omatyko@c2r6p11.hive.fi>
2024-12-02 17:43:46 +0200

    Header file added

---
+++ ft_printf.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omatyko <omatyko@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 17:34:43 by omatyko           #+#    #+#             */
+/*   Updated: 2024/12/02 17:43:06 by omatyko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+#include <unistd.h>
+int	prn_char(char c);
+int     prn_str(const char *str)
+#endif
